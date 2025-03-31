000f63194e39933f94980d8f7c9c52d48152a0cc
mmmv41 <tlsalsrud5068@daum.net>
2025-03-31 12:11:54 +0900

    dynamically typed language 공부

--- 14_js/01_javascript_core/01_variable/02_dynamically-typed-language/01_02_dynamically-typed-language.js
+++ 14_js/01_javascript_core/01_variable/02_dynamically-typed-language/01_02_dynamically-typed-language.js
@@ -1,5 +1,5 @@
 var test;
-console.log(typeof test);
+console.log(typeof test);       // undefined
 
 test = 1;
 console.log(typeof test);
@@ -8,16 +8,16 @@ test = 'JavaScript';
 console.log(typeof test);
 
 test = true;
-console.log(typeof test);
+console.log(typeof test);   
 
 test = Symbol();
-console.log(typeof test);
+console.log(typeof test);       // symbol
 
 test = {};
-console.log(typeof test);
+console.log(typeof test);       // object
 
 test = [];
-console.log(typeof test);
+console.log(typeof test);       // object
 
 test = function() {};
-console.log(typeof test);
+console.log(typeof test);       // function
